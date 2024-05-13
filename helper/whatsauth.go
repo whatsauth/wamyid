551fdbde12a8c26d1e65d0a0992adba5d519878d
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-13 23:33:24 +0700

    kasih tau gagal koneksi

--- helper/whatsauth.go
+++ helper/whatsauth.go
@@ -74,7 +74,10 @@ func HandlerIncomingMessage(msg model.IteungMessage, WAPhoneNumber string, db *m
 }
 
 func GetRandomReplyFromMongo(msg model.IteungMessage, db *mongo.Database) string {
-	rply, _ := GetRandomDoc[model.Reply](db, "reply", 1)
+	rply, err := GetRandomDoc[model.Reply](db, "reply", 1)
+	if err != nil {
+		return "Koneksi Database Gagal"
+	}
 	replymsg := strings.ReplaceAll(rply[0].Message, "#BOTNAME#", msg.Alias_name)
 	replymsg = strings.ReplaceAll(replymsg, "\\n", "\n")
 	return replymsg
