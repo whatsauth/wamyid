f7e6aac9a0e21a7db5304fe657b037985b86c67a
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-15 05:27:53 +0700

    remove yang tidak terpakai

--- controller/whatsauth.go
+++ controller/whatsauth.go
@@ -56,33 +56,3 @@ func NotFound(respw http.ResponseWriter, req *http.Request) {
 	resp.Response = "Not Found"
 	helper.WriteResponse(respw, http.StatusNotFound, resp)
 }
-
-func HandleRequest(respw http.ResponseWriter, req *http.Request) {
-	var resp model.Response
-	var msg model.IteungMessage
-	if helper.GetSecretFromHeader(req) == config.WebhookSecret {
-		err := json.NewDecoder(req.Body).Decode(&msg)
-		if err != nil {
-			resp.Response = err.Error()
-		} else {
-			resp, err = helper.WebHook(config.WAKeyword, config.WAPhoneNumber, config.WAAPIQRLogin, config.WAAPIMessage, msg, config.Mongoconn)
-			if err != nil {
-				resp.Response = err.Error()
-			}
-		}
-	} else {
-		dt := &model.WebHook{
-			URL:    config.WebhookURL,
-			Secret: config.WebhookSecret,
-		}
-		res, err := helper.RefreshToken(dt, config.WAPhoneNumber, config.WAAPIGetToken, config.Mongoconn)
-		if err != nil {
-			resp.Response = err.Error()
-		} else {
-			resp.Response = helper.Jsonstr(res.ModifiedCount)
-		}
-
-	}
-	resp.Info = req.Method + " " + req.URL.Path
-	helper.WriteResponse(respw, http.StatusOK, resp)
-}
