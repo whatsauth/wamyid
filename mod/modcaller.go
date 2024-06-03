276ef4e9d3873269311dd0f0ccb6a70af2f414d4
fahiraaa06 <fahira1678@gmail.com>
2024-06-03 16:19:21 +0700

    added: caller mod

--- mod/modcaller.go
+++ mod/modcaller.go
@@ -2,6 +2,7 @@ package mod
 
 import (
 	"github.com/gocroot/mod/idgrup"
+	"github.com/gocroot/mod/idname"
 	"github.com/whatsauth/itmodel"
 )
 
@@ -9,6 +10,8 @@ func Caller(Modulename string, Pesan itmodel.IteungMessage) (reply string) {
 	switch Modulename {
 	case "idgrup":
 		reply = idgrup.IDGroup(Pesan)
+	case "idname":
+		reply = idname.IDName(Pesan)
 	}
 	return
 }
