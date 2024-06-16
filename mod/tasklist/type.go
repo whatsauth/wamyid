24e9aa38bb22b8586ab8e09245fab9af130156b1
Rolly Maulana Awangga <awangga@gmail.com>
2024-06-17 06:47:17 +0700

    add tasklist

--- mod/tasklist/type.go
+++ mod/tasklist/type.go
@@ -4,10 +4,15 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type TaskList struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	MeetID      primitive.ObjectID `json:"meetid,omitempty" bson:"meetid,omitempty"`
 	LaporanID   primitive.ObjectID `json:"laporanid,omitempty" bson:"laporanid,omitempty"`
+	UserID      primitive.ObjectID `json:"userid,omitempty" bson:"userid,omitempty"`
 	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
 	PhoneNumber string             `json:"phonenumber,omitempty" bson:"phonenumber,omitempty"`
+	Email       string             `json:"email,omitempty" bson:"email,omitempty"`
 	Task        string             `json:"task,omitempty" bson:"task,omitempty"`
+	ProjectID   primitive.ObjectID `json:"projectid,omitempty" bson:"projectid,omitempty"`
+	ProjectName string             `json:"projectname,omitempty" bson:"projectname,omitempty"`
 	IsDone      bool               `json:"isdone,omitempty" bson:"isdone,omitempty"`
 }
 
