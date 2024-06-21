cbfbde32979c7be72e45a777ffedf56ceec870fd
Rolly Maulana Awangga <awangga@gmail.com>
2024-06-21 23:57:14 +0700

    tambah lastname

--- helper/telebot/type.go
+++ helper/telebot/type.go
@@ -8,6 +8,7 @@ type Update struct {
 			ID           int64  `json:"id"`
 			IsBot        bool   `json:"is_bot"`
 			FirstName    string `json:"first_name"`
+			LastName     string `json:"last_name"`
 			Username     string `json:"username"`
 			LanguageCode string `json:"language_code"`
 		} `json:"from"`
