0fcb4f7af95b53ba190c9cfd1286559b3215fd18
Rolly Maulana  Awangga <rolly@awang.ga>
2024-07-25 18:20:23 +0700

    GetHelpdeskName

--- mod/helpdesk/helpdesk.go
+++ mod/helpdesk/helpdesk.go
@@ -4,5 +4,5 @@ import "github.com/whatsauth/itmodel"
 
 func PenugasanOperator(Pesan itmodel.IteungMessage) (reply string) {
 
-	return "Selamat datang di HelpDesk Pamong Desa :\n" + Pesan.Group_id + "\nAnda kami hubungkan dengan operator kami Asep di nomor 62817898989\nTerima kasih"
+	return "Selamat datang di HelpDesk Pamong Desa :\n" + Pesan.Group_id + "\nAnda kami hubungkan dengan operator kami Asep di nomor wa.me/62817898989\nTerima kasih"
 }
--- mod/helpdesk/type.go
+++ mod/helpdesk/type.go
@@ -2,8 +2,6 @@ package helpdesk
 
 type Helpdesk struct {
 	Name         string   `json:"name,omitempty" bson:"name,omitempty"`
-	Keyword      []string `json:"keyword,omitempty" bson:"keyword,omitempty"`
 	Phonenumbers []string `json:"phonenumbers,omitempty" bson:"phonenumbers,omitempty"`
-	Group        bool     `json:"group,omitempty" bson:"group,omitempty"`
-	Personal     bool     `json:"personal,omitempty" bson:"personal,omitempty"`
+	Occupied     bool     `json:"occupied,omitempty" bson:"occupied,omitempty"`
 }
