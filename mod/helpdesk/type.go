1dcb19a59338a878562a34e4a1b237263524f0d5
Rolly Maulana Awangga <awangga@gmail.com>
2024-07-28 17:07:13 +0700

    tambah section dan chapter

--- mod/helpdesk/type.go
+++ mod/helpdesk/type.go
@@ -5,6 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Helpdesk struct {
 	Team          string `json:"team,omitempty" bson:"team,omitempty"`
 	Scope         string `json:"scope,omitempty" bson:"scope,omitempty"`
+	Section       string `json:"section,omitempty" bson:"section,omitempty"`
+	Chapter       string `json:"chapter,omitempty" bson:"chapter,omitempty"`
 	Name          string `json:"name,omitempty" bson:"name,omitempty"`
 	Phonenumbers  string `json:"phonenumbers,omitempty" bson:"phonenumbers,omitempty"`
 	JumlahAntrian int    `json:"jumlahantrian,omitempty" bson:"jumlahantrian,omitempty"`
