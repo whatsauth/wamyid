a5be0dd13a34309876be46842e1b2d19459481b4
fahiraaa06 <fahira1678@gmail.com>
2024-08-19 11:10:20 +0700

    update

--- mod/siakad/siakad.go
+++ mod/siakad/siakad.go
@@ -65,10 +65,10 @@ func extractRole(message string) string {
 }
 
 func extractProdi(message string) string {
-	re := regexp.MustCompile(`(?i)prodi:\s*([a-zA-Z0-9\s]+)`)
+	re := regexp.MustCompile(`prodi: (\S+)`)
 	matches := re.FindStringSubmatch(message)
 	if len(matches) > 1 {
-		return strings.TrimSpace(matches[1])
+		return matches[1]
 	}
 	return ""
 }
