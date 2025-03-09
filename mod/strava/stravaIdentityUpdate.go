e691195b29b307ddfd749f3495a5c79cf4892666
Febriand1 <fdirga63@gmail.com>
2025-03-10 00:30:43 +0700

    perbaiki logika dan penanganan kesalahan pada pembaruan identitas Strava

--- mod/strava/stravaIdentityUpdate.go
+++ mod/strava/stravaIdentityUpdate.go
@@ -11,16 +11,28 @@ import (
 )
 
 func StravaIdentityUpdateHandler(Pesan itmodel.IteungMessage, db *mongo.Database) string {
-	reply := "Informasi Profile Stava kamu: "
+	reply := "Informasi Profile Stava kamu1: "
+
+	col := "strava_identity"
+	data, err := atdb.GetOneDoc[StravaIdentity](db, col, bson.M{"phone_number": Pesan.Phone_number})
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return "Kak, akun Strava kamu belum terdaftar. Silakan daftar dulu!"
+		}
+		return "\n\nError fetching data dari MongoDB: " + err.Error()
+	}
+
+	if data.LinkIndentity == "" {
+		return "Kak, link Strava kamu belum tersimpan di database. Silakan tambahkan dulu!"
+	}
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.strava.com"),
 	)
 
-	var identities []StravaIdentity
-
 	stravaIdentity := StravaIdentity{}
-	stravaIdentity.AthleteId = athleteId
+	stravaIdentity.AthleteId = data.AthleteId
+	stravaIdentity.PhoneNumber = data.PhoneNumber
 
 	c.OnHTML("main", func(e *colly.HTMLElement) {
 		name := e.ChildText("h2.Details_name__Wz5bH")
@@ -31,49 +43,37 @@ func StravaIdentityUpdateHandler(Pesan itmodel.IteungMessage, db *mongo.Database
 				stravaIdentity.Picture = imgEl.Attr("src")
 			}
 		})
-
-		identities = append(identities, stravaIdentity)
-	})
-
-	col := "strava_identity"
-	data, err := atdb.GetOneDoc[StravaIdentity](db, col, bson.M{"phone_number": Pesan.Phone_number})
-	if err != nil && err != mongo.ErrNoDocuments {
-		return "\n\nError fetching data from MongoDB: " + err.Error()
-	}
-
-	c.OnScraped(func(r *colly.Response) {
-		if data.AthleteId == stravaIdentity.AthleteId {
-			if data.PhoneNumber != Pesan.Phone_number {
-				reply += "\n\nBukan akun kamu ini mah kak."
-				return
-			}
-
-			if data.Picture == stravaIdentity.Picture {
-				reply += "\n\nData Strava kak " + Pesan.Alias_name + " sudah up to date." + stravaIdentity.AthleteId
-				return
-			}
-
-			stravaIdentity.UpdatedAt = time.Now()
-
-			updateData := bson.M{
-				"picture":    stravaIdentity.Picture,
-				"updated_at": stravaIdentity.UpdatedAt,
-			}
-
-			_, err := atdb.UpdateDoc(db, col, bson.M{"athlete_id": stravaIdentity.AthleteId}, bson.M{"$set": updateData})
-			if err != nil {
-				reply += "\n\nError updating data to MongoDB: " + err.Error()
-				return
-			}
-
-			reply += "\n\nData kak " + Pesan.Alias_name + " sudah berhasil di update."
-
-		}
 	})
 
 	err = c.Visit(data.LinkIndentity)
 	if err != nil {
 		return "Link Profile Strava yang anda kirimkan tidak valid. Silakan kirim ulang dengan link yang valid.(3)"
+	}
+
+	if data.AthleteId == stravaIdentity.AthleteId {
+		if data.PhoneNumber != Pesan.Phone_number {
+			return "\n\nBukan akun kamu ini mah kak."
+		}
+
+		if data.Picture == stravaIdentity.Picture {
+			return "\n\nData Strava kak " + Pesan.Alias_name + " sudah up to date." + stravaIdentity.AthleteId
+		}
+
+		stravaIdentity.UpdatedAt = time.Now()
+
+		updateData := bson.M{
+			"picture":    stravaIdentity.Picture,
+			"updated_at": stravaIdentity.UpdatedAt,
+		}
+
+		_, err := atdb.UpdateDoc(db, col, bson.M{"athlete_id": stravaIdentity.AthleteId}, bson.M{"$set": updateData})
+		if err != nil {
+			return "\n\nError updating data to MongoDB: " + err.Error()
+
+		}
+
+		reply += "\n\nData kak " + Pesan.Alias_name + " sudah berhasil di update."
+
 	}
 
 	return reply
