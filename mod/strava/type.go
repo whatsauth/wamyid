0d86af95b37606e51341a903546fd9a06b7726a1
Febriand1 <fdirga63@gmail.com>
2025-03-10 15:02:10 +0700

    hapus omitempty

--- mod/strava/type.go
+++ mod/strava/type.go
@@ -5,24 +5,24 @@ import (
 )
 
 type StravaIdentity struct {
-	AthleteId     string    `bson:"athlete_id,omitempty" json:"athlete_id,omitempty"`
-	Picture       string    `bson:"picture,omitempty" json:"picture,omitempty"`
-	PhoneNumber   string    `bson:"phone_number,omitempty" json:"phone_number,omitempty"`
-	LinkIndentity string    `bson:"link_identity,omitempty" json:"link_identity,omitempty"`
+	AthleteId     string    `bson:"athlete_id" json:"athlete_id"`
+	Picture       string    `bson:"picture" json:"picture"`
+	PhoneNumber   string    `bson:"phone_number" json:"phone_number"`
+	LinkIndentity string    `bson:"link_identity" json:"link_identity"`
 	CreatedAt     time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt     time.Time `bson:"updated_at" json:"updated_at"`
 }
 
 type StravaActivity struct {
-	ActivityId   string    `bson:"activity_id,omitempty" json:"activity_id,omitempty"`
-	Picture      string    `bson:"picture,omitempty" json:"picture,omitempty"`
-	Name         string    `bson:"name,omitempty" json:"name,omitempty"`
-	Title        string    `bson:"title,omitempty" json:"title,omitempty"`
-	DateTime     string    `bson:"date_time,omitempty" json:"date_time,omitempty"`
-	TypeSport    string    `bson:"type_sport,omitempty" json:"type_sport,omitempty"`
-	Distance     string    `bson:"distance,omitempty" json:"distance,omitempty"`
-	MovingTime   string    `bson:"moving_time,omitempty" json:"moving_time,omitempty"`
-	Elevation    string    `bson:"elevation,omitempty" json:"elevation,omitempty"`
-	LinkActivity string    `bson:"link_activity,omitempty" json:"link_activity,omitempty"`
+	ActivityId   string    `bson:"activity_id" json:"activity_id"`
+	Picture      string    `bson:"picture" json:"picture"`
+	Name         string    `bson:"name" json:"name"`
+	Title        string    `bson:"title" json:"title"`
+	DateTime     string    `bson:"date_time" json:"date_time"`
+	TypeSport    string    `bson:"type_sport" json:"type_sport"`
+	Distance     string    `bson:"distance" json:"distance"`
+	MovingTime   string    `bson:"moving_time" json:"moving_time"`
+	Elevation    string    `bson:"elevation" json:"elevation"`
+	LinkActivity string    `bson:"link_activity" json:"link_activity"`
 	CreatedAt    time.Time `bson:"created_at" json:"created_at"`
 }
