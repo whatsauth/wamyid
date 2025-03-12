d891d32b0f337d08b6c93c5a4416e988d5dd1563
Febriand1 <fdirga63@gmail.com>
2025-03-12 18:00:55 +0700

    perbaiki kesalahan single struct menjadi slice struct

--- controller/strava.go
+++ controller/strava.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetStravaActivities(respw http.ResponseWriter, req *http.Request) {
-	doc, err := helper.GetAllDoc[strava.StravaActivity](config.Mongoconn, "strava_activity")
+	doc, err := helper.GetAllDoc[[]strava.StravaActivity](config.Mongoconn, "strava_activity")
 	if err != nil {
 		helper.WriteResponse(respw, http.StatusInternalServerError, err.Error())
 		return
