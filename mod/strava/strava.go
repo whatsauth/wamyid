2831d43ea69492954ce94850a53d8838b0482f60
Febriand1 <fdirga63@gmail.com>
2025-03-13 21:59:05 +0700

    selesai maintenance

--- mod/strava/strava.go
+++ mod/strava/strava.go
@@ -2,7 +2,7 @@ package strava
 
 var domApp = "strava.app.link"
 var domWeb = "www.strava.com"
-var isMaintenance = true
+var isMaintenance = false
 
 func maintenance(phone string) string {
 	if phone != "6282268895372" {
