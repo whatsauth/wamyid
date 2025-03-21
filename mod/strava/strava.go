61268b84313e89c7af540c260aedb2b9281db7a2
Febriand1 <fdirga63@gmail.com>
2025-03-21 23:30:17 +0700

    false

--- mod/strava/strava.go
+++ mod/strava/strava.go
@@ -8,7 +8,7 @@ import (
 
 var domApp = "strava.app.link"
 var domWeb = "www.strava.com"
-var isMaintenance = true
+var isMaintenance = false
 
 func maintenance(phone string) string {
 	if phone != "6282268895372" {
