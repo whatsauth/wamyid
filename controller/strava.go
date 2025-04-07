1c4f1fe7bfb9a1d498e1a5863d09e8f5a662b75f
Febriand1 <fdirga63@gmail.com>
2025-04-07 14:53:34 +0700

    add GetStravaActivitiesWithGrupIDFromPomokit

--- controller/strava.go
+++ controller/strava.go
@@ -1,10 +1,12 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gocroot/config"
 	"github.com/gocroot/helper"
+	"github.com/gocroot/mod/pomokit"
 	"github.com/gocroot/mod/strava"
 )
 
@@ -15,4 +17,46 @@ func GetStravaActivities(respw http.ResponseWriter, req *http.Request) {
 		return
 	}
 	helper.WriteResponse(respw, http.StatusOK, doc)
+}
+
+func GetStravaActivitiesWithGrupIDFromPomokit(respw http.ResponseWriter, req *http.Request) {
+	// Ambil data dari koleksi strava_activity
+	stravaDocs, err := helper.GetAllDoc[[]strava.StravaActivity](config.Mongoconn, "strava_activity")
+	if err != nil {
+		helper.WriteResponse(respw, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// Ambil data dari koleksi pomokit
+	pomokitDocs, err := helper.GetAllDoc[[]pomokit.PomodoroReport](config.Mongoconn, "pomokit")
+	if err != nil {
+		helper.WriteResponse(respw, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// Buat mapping phonenumber -> grup_id
+	phoneToGroup := make(map[string]string)
+	for _, p := range pomokitDocs {
+		if _, exists := phoneToGroup[p.PhoneNumber]; !exists {
+			phoneToGroup[p.PhoneNumber] = p.WaGroupID
+		}
+	}
+
+	// Gabungkan data tanpa mengubah struct
+	var response []map[string]interface{}
+	for _, s := range stravaDocs {
+		mergedData := StructToMap(s)                        // Convert struct ke map
+		mergedData["wagroupid"] = phoneToGroup[s.PhoneNumber] // Tambahkan grup_id
+		response = append(response, mergedData)
+	}
+
+	// Kirim respons
+	helper.WriteResponse(respw, http.StatusOK, response)
+}
+
+func StructToMap(data interface{}) map[string]interface{} {
+	jsonData, _ := json.Marshal(data)
+	var result map[string]interface{}
+	json.Unmarshal(jsonData, &result)
+	return result
 }
